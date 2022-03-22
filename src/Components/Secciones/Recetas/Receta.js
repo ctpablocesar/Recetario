8e768253caf35de956eaf71c260a7254464327d5
Pablo Cordova <Darckman201@gmail.com>
2022-03-22 17:31:26 -0600

    version 22/03/2022 P

--- src/Components/Secciones/Recetas/Receta.js
+++ src/Components/Secciones/Recetas/Receta.js
@@ -13,27 +13,28 @@ export const Receta = ({ receta }) => {
     return (
         <div className="card col-sm-5 tarjeta-noticias m-3">
             <div className="row no-gutters">
-                {/* <div className="col-sm-5 p-3">
+                <Link className='text-decoration-none text-body' to={`/receta/${id}`}>
+                    {/* <div className="col-sm-5 p-3">
                     <img className="card-img" src={imagen} alt="imagen del anuncio" />
                 </div> */}
-                <div className="col-sm-12">
-                    <div className="card-body">
-                        <h5 className="card-title"><strong>{titulo}</strong></h5>
-                        <p className="card-text"><strong>Descripcion: </strong>{newDescripcion}
-                            {
-                                descripcion.length > 30 &&
-                                <Link to={`/receta/${id}`}><span>...ver más</span></Link>
-                            }
-                        </p>
-                        <div className='etiquetas-receta'>
-                            <p className="card-text"><strong>Tipo: </strong>{ocacion}</p>
-                            <p className="card-text"><strong>Tiempo: </strong>{tiempo}</p>
+                    <div className="col-sm-12">
+                        <div className="card-body">
+                            <h5 className="card-title"><strong>{titulo}</strong></h5>
+                            <p className="card-text"><strong>Descripcion: </strong>{newDescripcion}
+                                {
+                                    descripcion.length > 30 &&
+                                    <span>...</span>
+                                }
+                            </p>
+                            <div className='etiquetas-receta'>
+                                <p className="card-text"><strong>Tipo: </strong>{ocacion}</p>
+                                <p className="card-text"><strong>Tiempo: </strong>{tiempo}</p>
+                            </div>
+                            <p className="card-text text-right"><small className="text-muted"> <Moment fromNow>{fecha}</Moment></small></p>
                         </div>
-                        <p className="card-text text-right"><small className="text-muted"> <Moment fromNow>{fecha}</Moment></small></p>
                     </div>
-                </div>
+                </Link>
             </div>
-          
         </div>
     )
 }
